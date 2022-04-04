81e617e0e3a13cb5d02be1b14f60314dccd4d7cc
Masoom Zaid <an-masoom@indicchain.com>
2022-04-04 11:18:55 +0530

    make api calls for meme app

--- app/src/main/java/com/example/memeapp/MainActivity.kt
+++ app/src/main/java/com/example/memeapp/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.memeapp
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Request.Method.GET
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 import com.example.memeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +21,32 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-
-
         setContentView(view)
+
+        fetchMemes()
+
+        binding.btnShare.setOnClickListener {
+            fetchMemes()
+        }
+    }
+
+    private fun fetchMemes(){
+        val queue = Volley.newRequestQueue(this)
+
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        val jsonObjectRequest = JsonObjectRequest(GET, url , null , {
+            try{
+                binding.txTitle.text = it.getString("title")
+                Glide.with(this).load(it.getString("url")).into(binding.image)
+            }catch (e : Exception){
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+
+        }, {
+            Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+        })
+
+        queue.add(jsonObjectRequest)
     }
 }
